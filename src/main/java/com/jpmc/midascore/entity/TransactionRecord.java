7da14fdfa248d585e09fedaef7ee8a2fce0de449
Roch APETOH D. <rapetoh@hotmail.com>
2025-09-23 20:32:45 -0500

    Created the TransactionRecord.java file

---
+++ src/main/java/com/jpmc/midascore/entity/TransactionRecord.java
@@ -0,0 +1,56 @@
+package com.jpmc.midascore.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Entity
+public class TransactionRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private UserRecord sender;
+
+    @ManyToOne
+    private UserRecord recipient;
+
+    private double amount;
+
+    // Getter AND Setter
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UserRecord getSender() {
+        return sender;
+    }
+
+    public void setSender(UserRecord sender) {
+        this.sender = sender;
+    }
+
+    public UserRecord getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(UserRecord recipient) {
+        this.recipient = recipient;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
